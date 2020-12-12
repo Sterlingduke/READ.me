684151d967574fc4540c18d596b573c16ce86736
SterlingDuke <sterlingcduke@gmail.com>
2020-12-11 23:52:29 -0600

    loaded all needed files, and start of index.js/markdown.js

---
+++ generateMarkdoen.js
@@ -0,0 +1,45 @@
+function generateMarkdown(response) {
+    return `
+  
+    # ${response.projectTitle}
+    
+    # Table of Content
+    -[description](#description)
+    -[installation](#installation)
+    -[usage](#usage)
+    -[licenses](#licenses)
+    -[contribution](#contribution)
+    -[test](#test)
+    -[username](#username)
+    -[profile](#profile)
+    
+    ${response.username}
+    ##username:
+    
+        ${response.description}
+    ##description:
+    
+        ${response.installation}
+    ##installation:
+    
+        ${response.usage}
+    ##usage:
+    
+        ${response.licenses}
+    ##licenses:
+    
+        ${response.contribution}
+    ##contribution:
+    
+        ${response.test}
+    ##test:
+    
+        ${response.email}
+    ##email:
+    
+        ${response.questions}
+    ##profile:
+    `;
+    }
+    
+    module.exports = generateMarkdown;
